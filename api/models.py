bd3f2cd028c365161aafc4e9b5d9239b6955036e
Dimitriy <nationmdxr@gmail.com>
2024-11-19 03:19:29 +0300

    add Environment model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Environment(models.Model):
+    name = models.CharField(max_length = 48)
+    description = models.CharField(max_length = 256, blank = True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    createdAt = models.DateTimeField(auto_now_add=True)
+    editedAt = models.DateTimeField(auto_now=True)
