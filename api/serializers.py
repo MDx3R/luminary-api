217315ca194fe2c62ea0879496ff01f43b34441a
Dimitriy <nationmdxr@gmail.com>
2024-11-24 16:23:54 +0300

    add User, Environment and File serializers

---
+++ api/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+from .models import Environment
+
+# Create your serializers here.
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password']
+        extra_kwargs = {
+            'id': {'read_only': True},
+            'password': {'write_only': True}
+        }
+
+class EnvironmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Environment
+        fields = ['id', 'name', 'description', 'user', 'createdAt', 'editedAt']
+        extra_kwargs = {
+            'id': {'read_only': True}
+        }
+
+class FileSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=50)
+    file = serializers.FileField()
