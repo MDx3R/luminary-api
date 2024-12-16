5e1ee2e7a0f88beebf1ec921883e3981804ce2af
Dimitriy <nationmdxr@gmail.com>
2024-12-16 13:09:01 +0300

    add makePath method to conveniently create paths and refactor code a bit

--- api/managers.py
+++ api/managers.py
@@ -22,7 +22,7 @@ class FileManager(ABC):
     def list(self, path: str) -> List[str]:
         """Выводит список имен в директории, указанной в path"""
         pass
-    
+
     @abstractmethod
     def listFiles(self, path: str) -> List[str]:
         """Выводит список имен файлов в директории, указанной в path"""
@@ -95,16 +95,19 @@ class LocalFileManager(FileManager):
                     os.mkdir(path=x)
                 currentPath+='/'
 
+    def makePath(self, path: str, name: str = "") -> str:
+        return f'{self.basePath}/{path}{f"/{name}" if name else ""}'
+
     def list(self, path: str) -> List[str]:
         """Выводит список имен в директории, указанной в path"""
 
-        return os.listdir(f'{self.basePath}/{path}')
-    
+        return os.listdir(self.makePath(path))
+
     def listFiles(self, path: str) -> List[str]:
         """Выводит список имен файлов в директории, указанной в path"""
         result = []
         for name in self.list(path):
-            if (name.count('.') == 0): # This means name is dir
+            if (os.path.isdir(self.makePath(path, name))): # This means name is dir
                 continue
 
             result.append(name)
@@ -115,54 +118,65 @@ class LocalFileManager(FileManager):
         """Выводит список сведений о файлах в директории, указанной в path"""
         result = []
         for name in self.list(path):
-            if (name.count('.') == 0): # This means name is dir
+            fullPath = self.makePath(path, name)
+            if (os.path.isdir(fullPath)): # This means name is dir
                 continue
 
-            stat = os.stat(f'{self.basePath}/{path}/{name}')
-            result.append(fileStatFactory(name, stat.st_size, int(stat.st_mtime)))
+            stat = os.stat(fullPath)
+            result.append(
+                fileStatFactory(
+                    name, 
+                    stat.st_size, 
+                    int(stat.st_mtime)
+                )
+            )
 
         return result
 
     def exists(self, path: str) -> bool:
         """Проверяет существование директории или файл по пути path"""
-        return os.path.exists(f'{self.basePath}/{path}')
+        return os.path.exists(self.makePath(path))
 
     def makeDir(self, path: str) -> None:
         """Создает директорию, указанную в path"""
-        os.mkdir(f'{self.basePath}/{path}')
+        os.mkdir(self.makePath(path))
 
     def removeDir(self, path: str) -> None:
         """Удаляет директорию, указанную в path"""
-        os.rmdir(f'{self.basePath}/{path}')
+        os.rmdir(self.makePath(path))
 
     def clearDir(self, path: str) -> None:
         """Очищает директорию, указанною в path"""
-        pass
+        for name in self.list(path):
+            fullPath = self.makePath(path, name)
+            if (os.path.isdir(fullPath)):
+                self.removeDir()
+            else:
+                self.removeFile(fullPath)
 
     def readFile(self, path: str, name: str) -> str:
         """Читает файл с именем name в директории path"""
-        with open(f'{self.basePath}/{path}/{name}', "r", encoding="utf-8") as file:
+        with open(self.makePath(path, name), "r", encoding="utf-8") as file:
             return file.read()
         
     def saveFile(self, path: str, name: str, data: str) -> None:
         """
         Создает файл с именем name в директории path и записывает в него data целиком
         """
-        with open(f'{self.basePath}/{path}/{name}', "wb+") as file:
+        with open(self.makePath(path, name), "wb+") as file:
             file.write(data)
 
     def saveFileByChunks(self, path: str, name: str, data: Iterator[bytes]) -> None:
         """
         Создает файл с именем name в директории path и записывает в него data по частям
         """
-        with open(f'{self.basePath}/{path}/{name}', "wb+") as file:
+        with open(self.makePath(path, name), "wb+") as file:
             for x in data:
                 file.write(x)
 
     def removeFile(self, path: str, name: str) -> None:
         """Удаляет файл с именем name в директории path"""
-        print(f'{self.basePath}/{path}/{name}')
-        os.remove(f'{self.basePath}/{path}/{name}')
+        os.remove(self.makePath(path, name))
 
 class RemoteFileManager(FileManager):
     """
